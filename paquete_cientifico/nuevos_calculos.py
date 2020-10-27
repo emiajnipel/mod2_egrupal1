8bb0144e76434955c28811ed517fadb2857177d2
jaime lepin-fernandez <73120594+emiajnipel@users.noreply.github.com>
2020-10-27 11:57:58 -0300

    actualización_2

---
+++ paquete_cientifico/nuevos_calculos.py
@@ -0,0 +1,18 @@
+
+# área de un rectangulo, dados sus lados
+
+def area_rect(cara_1, cara_2):
+    area_rectangular = cara_1*cara_2
+    return area_rectangular
+
+# perímetro de un rectangulo, dados sus lados
+
+def perim_rect(cara_1, cara_2):
+    perimetro_rectangular = cara_1*2+cara_2*2
+    return (perimetro_rectangular)
+
+# distancia recorrida, dado tiempo y velocidad.
+
+def distancia_rec(velocidad, tiempo):
+    distancia = velocidad*tiempo
+    return distancia
